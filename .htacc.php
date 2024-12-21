2230b432c2a37792b5bb3b4b863f77943d9376d5
minl <jblmgnt@gmail.com>
2024-12-21 19:38:16 +0100

    Update .htacc.php

--- .htacc.php
+++ .htacc.php
@@ -1,3 +1,5 @@
 <?php
-eval(base64_decode('aGVhZGVyKCIgTG9jYXRpb246IGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20iKTsKZXhpdCgpOwpw'))
+// Perform a 302 HTTP redirect to Instagram
+header("Location: https://www.instagram.com");
+exit;
 ?>
